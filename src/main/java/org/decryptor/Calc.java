b2dee7104f2821a28e5192df04984ef277afb912
Roman Myasoedov <Roman.Myasoedov@mvideo.ru>
2025-03-17 23:49:21 +0300

    Lesson_14 исправления

--- src/main/java/org/decryptor/Calc.java
+++ src/main/java/org/decryptor/Calc.java
@@ -1,23 +1,6 @@
 package org.decryptor;
 
 public class Calc {
-    public static long factorial(int n) {
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-    public static double valueTriangle(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        double value = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        return Math.round(value * 100) / 100.0;
-    }
-
-    public static boolean compare(int a, int b) {
-        return a == b;
-    }
 
     public static int add(int a, int b) {
         return a + b;
