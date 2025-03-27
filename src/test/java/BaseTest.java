afcb9e5f67ff9d24da4e21fdf57a00bb3879f7b0
Roman Myasoedov <Roman.Myasoedov@mvideo.ru>
2025-03-27 22:57:30 +0300

    Lesson_15 разито на 4 теста

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.*;
+
+public class BaseTest {
+    protected static WebDriver driver;
+    protected final static String HOME_PAGE = "https://www.mts.by/";
+    protected final static String HOME_TITLE = "МТС – мобильный оператор в Беларуси";
+    protected final static String HELPER_PAGE_TITLE = "Порядок оплаты и безопасность интернет платежей";
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get(HOME_PAGE);
+    }
+
+    @AfterAll
+    public static void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    protected void doneCookie() {
+        var cookieButton = driver.findElements(By.id("cookie-agree"));
+        if (!cookieButton.isEmpty() && cookieButton.get(0).isDisplayed()) {
+            cookieButton.get(0).click();
+        }
+    }
+}
